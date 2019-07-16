ac4d235ef19539bdeae93746633ff6cfaacf6319
Tracy Reuther <tracyreuther@gmail.com>
2019-07-16 01:07:49 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
 
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
-
+doc.css(".site-header__hero__headline")
